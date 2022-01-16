5f885c5d56db86d5d4cb4a9bb74624b85b49a166
Tedrayreq <teddu12@hotmail.fr>
2022-01-16 23:24:26 +0100

    clean pour rendu final

--- V2/csvfolder/mypath.py
+++
@@ -1,8 +0,0 @@
-import os.path
-# import os
-# print(os.path.dirname(sys.argv[0]))
-
-# filename = "test.csv"
-chemin = 'test.csv'
-isFile = os.path.exists('test.txt')
-print(isFile)
--- V2/mypath.py
+++
@@ -1,7 +0,0 @@
-import os.path
-from os import path
-import sys
-print(os.path.dirname(sys.argv[0]))
-
-filename = 'test.csv'
-print(os.path.exists(filename))
